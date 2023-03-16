5b3a720adf4b304c42f83f7ee602295cae2c25ab
kiku99 <elsd0326@gmail.com>
2023-03-16 18:20:22 +0900

    first init

--- application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.cluster-cce3f85whuot.ap-northeast-2.rds.amazonaws.com',
-    DB_USER : 'kiku',
-    DB_PWD : 'rkp774910.',
-    DB_DATABASE : 'webappdb'
-});2
